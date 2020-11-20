9c138ff51fdebaf6dccd41879fe7180c929daad2
Nicole Uva <nicouva@gmail.com>
2020-11-19 23:05:55 -0500

    md commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# sjfl
-
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  # Read Me
+  Read Me Generator
+  ***
+  ## Table of Contents
+  [Installation Instructions](#installation-instructions)
+  [Usage Description](#license-information)
+  [Contribution Instructions](#contribution-instructions)
+  [Testing Procedure](#testing-procedure)
+  [Questions](#questions)
+  ##Installations Instructions
+  Please clone the repo. Be sure to npm install on your terminal. npm init
+  ##Usage Description
+  Please run node index.js
+  ##License Information
+  MIT
+  ##Contribution Instructions
+  Please reach out for collaboration
+  ##Testing Procedures
+  undefined
+  ##Questions
+  If you have any questions, you can reach me at nicouva@gmail.com and please follow me on GitHub at https://github.com/nicouva
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-  
+  const badges = ['![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)', '(https://opensource.org/licenses/MIT)', '![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)', '(https://opensource.org/licenses/MPL-2.0)', '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)', '(https://opensource.org/licenses/Apache-2.0)']
+  let badge = ''
 
-
-
-
-  return `# ${data.title}
-
-`;
+  if (data.license === 'MIT') {
+    badge = badges[0]
+  } else if (data.license === 'MPL') {
+    badge = badges[1]
+  } else if (data.license === 'Apache') {
+    badge = badges[2]
+  }
+  return `${badge}
+  # ${data.title}
+  ${data.description}
+  ***
+  ## Table of Contents
+  [Installation Instructions](#installation-instructions)
+  [Usage Description](#license-information)
+  [Contribution Instructions](#contribution-instructions)
+  [Testing Procedure](#testing-procedure)
+  [Questions](#questions)
+  ##Installations Instructions
+  ${data.installation}
+  ##Usage Description
+  ${data.usage}
+  ##License Information
+  ${data.license}
+  ##Contribution Instructions
+  ${data.contributions}
+  ##Testing Procedures
+  ${data.test}
+  ##Questions
+  If you have any questions, you can reach me at ${data.email} and please follow me on GitHub at https://github.com/${data.username}`;
 }
 
 module.exports = generateMarkdown;
